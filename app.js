f412f6bc024515c66d151c1307accfec7889461e
Pasha <pashaalbara01@gmail.com>
2023-05-23 22:36:32 +0700

    line animation

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 AOS.init()
+
+const scrollPage = () => {
+    const bodyST = document.body.scrollTop;
+    const docST = document.documentElement.scrollTop;
+    const docSH = document.documentElement.scrollHeight;
+    const docCH = document.documentElement.clientHeight;
+
+  return (docST + bodyST) / (docSH - docCH) * 100
+}
+window.onscroll = () => {
+    lineAnimation.seek((scrollPage() / 210) * lineAnimation.duration);
+};
+
+const lineAnimation = anime({
+    targets: '#lineAnimation',
+    height: ['0%','100%'],
+    easing: 'easeInOutQuart',
+    direction: 'alternate',
+    autoplay: false,
+});
